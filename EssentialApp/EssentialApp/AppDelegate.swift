6f11c128305efc14fc09b8016f803cc5c4d8cea5
Thiago Penna <thiago.penna@icloud.com>
2021-07-27 13:56:47 -0300

    Add commented-out applicationDidReceiveMemoryWarning implementation

--- EssentialApp/EssentialApp/AppDelegate.swift
+++ EssentialApp/EssentialApp/AppDelegate.swift
@@ -5,4 +5,16 @@
 import UIKit
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {}
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func applicationDidReceiveMemoryWarning(_ application: UIApplication) {
+        
+        print("### APPLICATION received memory warning")
+        
+//        if let navigationController = application.windows.first?.rootViewController as? UINavigationController {
+//            let oldVCs = navigationController.viewControllers
+//            let newVCs = [oldVCs.first, oldVCs.last].compactMap { $0 }
+//            navigationController.setViewControllers(newVCs, animated: false)
+//        }
+    }
+    
+}
